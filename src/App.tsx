5d35dd8a6de1c08a0d3b3ec631c8f49a46c64215
tigadika <amardika.noor@gmail.com>
2024-12-17 00:14:38 +0700

    refactor: experience in UI

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Navbar from "./ui/Navbar";
 import Header from "./ui/Header";
 
 import TechStacks from "./ui/TechStacks";
-import ExperienceContainer from "./app/experience/ExperienceContainer";
+import ExperienceContainer from "./ui/experience/ExperienceContainer";
 import PastProjectContainer from "./ui/past-projects/PastProjectContainer";
 import CtaFooter from "./ui/CtaFooter";
 import Footer from "./ui/Footer";
