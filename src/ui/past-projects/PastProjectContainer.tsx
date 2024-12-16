257f7d6276d68e3883ccb4b86a4fa0709a58729b
tigadika <amardika.noor@gmail.com>
2024-12-16 23:00:35 +0700

    fix motion in projects modal

--- src/ui/past-projects/PastProjectContainer.tsx
+++ src/ui/past-projects/PastProjectContainer.tsx
@@ -9,6 +9,7 @@ import {
 import { PAST_PROJECTS, PastProject } from "./data";
 import { cn } from "../../lib/utils";
 import { FaLink, FaX } from "react-icons/fa6";
+import { AnimatePresence, motion } from "motion/react";
 
 export default function PastProjectContainer() {
   const data = PAST_PROJECTS;
@@ -47,8 +48,9 @@ export default function PastProjectContainer() {
       <div className="mx-auto mb-20 grid w-2/3 grid-cols-1 place-items-center gap-6 md:grid-cols-2 lg:grid-cols-3">
         {data &&
           data.map((project, i) => (
-            <div
+            <motion.div
               key={i}
+              layoutId={`project-${i}`}
               className={cn(
                 isInPattern(i) ? "lg:col-span-2" : "lg:col-span-1",
                 "group relative w-full cursor-pointer overflow-hidden rounded-md bg-zinc-300",
@@ -57,42 +59,71 @@ export default function PastProjectContainer() {
                 openModal(project);
               }}
             >
-              <div className="absolute left-5 top-5 z-30 text-[2rem] font-semibold tracking-tight text-zinc-100">
+              <motion.div className="absolute left-5 top-5 z-30 text-[2rem] font-semibold tracking-tight text-zinc-100">
                 {project.name}
-              </div>
-              <div className="absolute h-full w-full bg-gradient-to-b from-purple-800 to-zinc-200 opacity-60"></div>
-              <img
-                src={project.imgUrl}
-                alt=""
-                className="h-[18rem] w-full object-cover object-left-top"
-              />
-            </div>
+              </motion.div>
+              <motion.div className="absolute h-full w-full bg-gradient-to-b from-purple-800 to-zinc-200 opacity-60"></motion.div>
+              <motion.div>
+                <img
+                  src={project.imgUrl}
+                  alt=""
+                  className="h-[18rem] w-full object-cover object-left-top"
+                />
+              </motion.div>
+            </motion.div>
           ))}
       </div>
-      <PastProjectModal isOpen={isModalOpen} onClose={closeModal}>
-        <img src={openedProject?.imgUrl} alt="" />
-        <div className="flex flex-col gap-3 px-6 py-2 text-zinc-200">
-          <h3 className="text-lg font-semibold">{openedProject?.name}</h3>
-          <a
-            href={openedProject?.links}
-            target="_blank"
-            className="font-semibold text-pink-500 hover:text-purple-500"
+      <AnimatePresence>
+        {openedProject && isModalOpen && (
+          <PastProjectModal
+            isOpen={isModalOpen}
+            onClose={closeModal}
+            projectIndex={data.indexOf(openedProject)}
           >
-            <FaLink className="inline" /> : {openedProject?.links}
-          </a>
-          <pre className="block text-pretty text-justify font-sans">
-            {openedProject?.description}
-          </pre>
-          <div className="flex flex-wrap gap-2">
-            {openedProject &&
-              openedProject.tags.map((tag, i) => (
-                <div className="tracking-tight-tracking-tight cursor-default rounded-md border border-pink-500 px-1 text-sm text-pink-500 hover:bg-pink-500 hover:text-white">
-                  {tag}
-                </div>
-              ))}
-          </div>
-        </div>
-      </PastProjectModal>
+            <motion.div layoutId={`project-${data.indexOf(openedProject)}`}>
+              <img
+                src={openedProject?.imgUrl}
+                alt=""
+                className="object-cover"
+              />
+            </motion.div>
+            <motion.div
+              className="flex flex-col gap-3 px-6 py-2 text-zinc-200"
+              layoutId={`project-content-${data.indexOf(openedProject)}`}
+              initial={{ opacity: 0, y: -50 }}
+              animate={{
+                opacity: 1,
+                y: 0,
+                transition: { duration: 0.3, delay: 0.5 },
+              }}
+              exit={{ opacity: 0, y: -50, transition: { duration: 0.3 } }}
+            >
+              <h3 className="text-lg font-semibold">{openedProject?.name}</h3>
+              <a
+                href={openedProject?.links}
+                target="_blank"
+                className="font-semibold text-pink-500 hover:text-purple-500"
+              >
+                <FaLink className="inline" /> {openedProject?.links}
+              </a>
+              <pre className="block text-pretty text-justify font-sans">
+                {openedProject?.description}
+              </pre>
+              <motion.div className="flex flex-wrap gap-2">
+                {openedProject &&
+                  openedProject.tags.map((tag, i) => (
+                    <motion.div
+                      key={i}
+                      className="tracking-tight-tracking-tight cursor-default rounded-md border border-pink-500 px-1 text-sm text-pink-500 hover:bg-pink-500 hover:text-white"
+                    >
+                      {tag}
+                    </motion.div>
+                  ))}
+              </motion.div>
+            </motion.div>
+          </PastProjectModal>
+        )}
+      </AnimatePresence>
     </>
   );
 }
@@ -101,10 +132,12 @@ function PastProjectModal({
   isOpen,
   onClose,
   children,
+  projectIndex,
 }: {
   isOpen: boolean;
   onClose: () => void;
   children: ReactNode;
+  projectIndex: number;
 }) {
   const dialogRef = useRef<HTMLDialogElement>(null);
   const contentRef = useRef<HTMLDivElement>(null);
@@ -143,12 +176,16 @@ function PastProjectModal({
     }
   };
   return (
-    <dialog
+    <motion.dialog
       ref={dialogRef}
       onClose={handleClose}
       onKeyDown={handleKeyDown}
       onClick={handleDialogClick}
-      className="w-full max-w-xl rounded-lg bg-white shadow-xl backdrop:bg-zinc-900 backdrop:opacity-80"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 0.2, delay: 0.1 }}
+      className="scrollbar w-full max-w-xl rounded-lg bg-white shadow-xl backdrop:bg-zinc-900 backdrop:opacity-80"
     >
       <div ref={contentRef} className="modal-content relative bg-zinc-800">
         <div className="absolute right-4 top-4">
@@ -167,6 +204,6 @@ function PastProjectModal({
           </button>
         </div>
       </div>
-    </dialog>
+    </motion.dialog>
   );
 }
